52890b63fd8f55972c667a4747d5587f15aca786
Andreas Östling <andreas.ostling@ur.se>
2021-08-27 15:28:05 +0200

    fix failing test

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from "./App";
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(
-    /Jennie och Andreas Pipeline lyckades!/i
+    /Jennie och Andreas Pipeline lyckades, nu kör vi en demo!/i
   );
   expect(linkElement).toBeInTheDocument();
 });
